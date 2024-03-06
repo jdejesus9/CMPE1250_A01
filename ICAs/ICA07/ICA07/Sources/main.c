e08f9cfde8f4c942091a24a8903de4d49fa692c7
jdejesus9 <143832361+jdejesus9@users.noreply.github.com>
2024-03-06 09:43:11 -0700

    commit

--- ICAs/ICA07/ICA07/Sources/main.c
+++ ICAs/ICA07/ICA07/Sources/main.c
@@ -1,10 +1,10 @@
 /********************************************************************/
-// HC12 Program:  YourProg - MiniExplanation
+// HC12 Program:  ICA07 - RTI Library and Blocking Delays
 // Processor:     MC9S12XDP512
 // Bus Speed:     40MHz
 // Author:        JACKELYN DE JESUS
 // Details:       A more detailed explanation of the program is entered here               
-// Date:          Date Created
+// Date:          March 06, 2024
 // Revision History :
 //  each revision will have a date + desc. of changes
 
@@ -20,6 +20,10 @@
 //#include <stdlib.h>
 //#include <stdio.h>
 
+#include "sw_led.h"
+#include "pll.h"
+#include "rti.h"
+#include "clock.h"
 
 /********************************************************************/
 //Defines
@@ -28,7 +32,7 @@
 /********************************************************************/
 // Local Prototypes
 /********************************************************************/
-
+interrupt VectorNumber_Vrti void RTI_1ms (void);
 /********************************************************************/
 // Global Variables
 /********************************************************************/
@@ -46,12 +50,16 @@ void main(void)
 
   // main entry point
   _DISABLE_COP();
-  //EnableInterrupts;
+  EnableInterrupts;
   
 /********************************************************************/
   // one-time initializations
 /********************************************************************/
-
+RTI_Init();
+SWL_Init();
+RTICTL = 0b10010111;
+//STEP 2 - ENABLE RTI
+CRGINT |= CRGINT_RTIE_MASK;
 
 /********************************************************************/
   // main program loop
@@ -59,14 +67,35 @@ void main(void)
 
   for (;;)
   {
+  //   //Part 2
+  //   //RTI_Delay_ms(100);
+  //   //SWL_TOG(SWL_GREEN);
 
+  //  //Part 3
+  //   RTI_Delay_ms(1000);
+  //   if(SWL_Pushed(SWL_LEFT)) //if Left switch is pushed, then toggle the yellow led, else toggle green led for part c
+  //   {
+  //   SWL_OFF(SWL_GREEN);
+  //   SWL_TOG(SWL_YELLOW);
+  //   }
+  //   else{
+  //    SWL_OFF(SWL_YELLOW);		
+  //   SWL_TOG(SWL_GREEN);
+  //   }
   }                   
 }
 
 /********************************************************************/
 // Functions
 /********************************************************************/
-
+interrupt VectorNumber_Vrti void RTI_1ms (void)
+{
+ CRGFLG = CRGFLG_RTIF_MASK; //clear flag;
+ //Perform some action here
+  //Part 1
+  SWL_TOG(SWL_RED);
+  rtiMasterCount++;
+}
 /********************************************************************/
 // Interrupt Service Routines
 /********************************************************************/
