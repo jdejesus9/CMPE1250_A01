7575c12d9bbbf71b44d13f04753b1061f44cb6ba
jdejesus9 <jdejesus9@nait.ca>
2024-04-12 13:48:13 -0600

    ica09 commit

--- ICAs/ICA09/ICA09/Sources/main.c
+++ ICAs/ICA09/ICA09/Sources/main.c
@@ -37,7 +37,14 @@
 /********************************************************************/
 // Global Variables
 /********************************************************************/
-
+int i;
+int j;
+int sum;
+unsigned char uString[25];
+unsigned char lString[25];
+char uppercasevowels[] = {'A', 'E', 'I', 'O', 'U'};
+char lowercasevowels[] = {'a', 'e', 'i', 'o', 'u'};   
+char emptyArr[25];
 /********************************************************************/
 // Constants
 /********************************************************************/
@@ -61,14 +68,52 @@ Clock_Set20MHZ();
 
 PLL_To20MHz();
 
-sci0_InitMath(200000,19200);
+sci0_InitMath(20000000,19200);
+
+SCI0CR2 = 0b00001100;
 
 /********************************************************************/
   // main program loop
 /********************************************************************/
+scio_txStr("\x1b[31m \x1b[0;0H Jackelyn De Jesus");
 
   for (;;)
   {
+    RTI_Delay_ms(250);
+    SWL_TOG(SWL_RED)
+
+    if (SWL_Pushed(SWL_CTR))
+    {
+      sum = 0;
+      for (i = 0; i < 20; i++)
+      {
+        uString[i] = uppercasevowels[(int)rand() % 5];
+        sum += uString[i];
+      }
+
+      sci0_txStr("\x1b[2;2H \x1b[32m VOWELS = ");
+      sprintf("\x1b[2;2H \x1b[32m VOWELS = ", uString);
+      sci0_txStr(uString);
+
+      sprintf(emptyArr, "\x1b[3;3H \x1b[33m ASCII SUM = %d", sum);
+      sci0_txStr(emptyArr);
+    }
+    else{
+      sum = 0;
+
+      for (j = 0; j < 20; j++)
+      {
+        lString[j] = lowercasevowels[(int)rand() % 5];
+        sum += lString[j];
+      }
+
+      sci0_txStr("\x1b[2;2H \x1b[32m VOWELS = ");
+      sprintf("\x1b[2;2H \x1b[32m VOWELS = ", uString);
+      sci0_txStr(lString);
+
+      sprintf(emptyArr, "\x1b[3;3H \x1b[33m ASCII SUM = %d", sum);
+      sci0_txStr(emptyArr);
+    }
     
   }                   
 }
